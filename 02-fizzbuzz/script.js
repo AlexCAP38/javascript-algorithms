fc49ca94c074bcc510b26f50d382f237cc339b8a
Alexey Gribov <ultraangarsk@gmail.com>
2023-08-14 20:12:31 +0700

    14.08.23

--- 02-fizzbuzz/script.js
+++ 02-fizzbuzz/script.js
@@ -11,9 +11,39 @@
 */
 
 function fizzBuzz(num) {
-    // Напишите код здесь
-}
+    //    if (typeof num !== 'undefined') return console.log('Аргумент не число');
+    if (num % 3 === 0 && num % 5 === 0) {
+        console.log(num);
+        return console.log('fizzbuzz');
+    };
 
+    if (num % 3 === 0) {
+        console.log(num);
+        return console.log('fizz');
+    };
+
+    if (num % 5 === 0) {
+        console.log(num);
+        return console.log('fizz');
+    }
+    console.log(num);
+    return num
+}
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
+console.log(fizzBuzz(1));
+console.log(fizzBuzz(2));
+console.log(fizzBuzz(3));
+console.log(fizzBuzz(4));
+console.log(fizzBuzz(5));
+console.log(fizzBuzz(6));
+console.log(fizzBuzz(7));
+console.log(fizzBuzz(8));
+console.log(fizzBuzz(9));
+console.log(fizzBuzz(10));
 console.log(fizzBuzz(15));
+// console.log(fizzBuzz(-20));
+// console.log(fizzBuzz(18.22));
+// console.log(fizzBuzz(10 / 0));
+// console.log(fizzBuzz('ssss'));
+// console.log(fizzBuzz(15));
